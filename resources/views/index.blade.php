207b889612945698650bb2442b13631fd790c872
Mutonga <ngshvill57@gmail.com>
2022-09-20 22:05:51 +0300

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Synchroconsult Associates LTD</title>
+
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+		<meta name="format-detection" content="telephone=no">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+
+		{{-- bootstrap cdn --}}
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+		{{-- fontawesome cdn --}}
+		<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+		<!-- Fonts-->
+		<link rel="stylesheet" type="text/css" href="{{ asset('fonts/fontawesome/font-awesome.min.css')}}">
+		
+		<!-- App & fonts-->
+		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Work+Sans:300,400,500,700">
+		<link rel="stylesheet" type="text/css" id="app-stylesheet" href="{{ asset('css/main.css')}}"><!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<![endif]-->
+	</head>
+	
+	<body>
+		<div class="" id="root">
+			<!-- Content-->
+			<div class="md-content">
+				
+				<!-- hero -->
+				<div class="hero md-skin-dark">
+					<!-- header -->
+					<div class="header">
+						<div class="header__left">
+							<span>Roughton Court Plaza, Argwings Kodhek Road Nairobi</span>
+							<span>020 2713031</span>
+							<span>info@synchroconsult.co.ke</span>
+						</div>
+						<div class="header__social">
+							
+							<!-- social-icon -->
+							<a class="social-icon social-icon__rounded" href="#"><i class="fa fa-facebook"></i>
+							</a><!-- End / social-icon -->
+							
+							
+							<!-- social-icon -->
+							<a class="social-icon social-icon__rounded" href="#"><i class="fa fa-twitter"></i>
+							</a><!-- End / social-icon -->
+							
+							
+							<!-- social-icon -->
+							<a class="social-icon social-icon__rounded" href="#"><i class="fa fa-behance"></i>
+							</a><!-- End / social-icon -->
+							
+							
+							<!-- social-icon -->
+							<a class="social-icon social-icon__rounded" href="#"><i class="fa fa-google-plus"></i>
+							</a><!-- End / social-icon -->
+							
+						</div>
+					</div><!-- End / header -->
+					
+					<div class="container">
+						<div class="hero__wrapper">
+							<div class="row">
+								<div class="col-lg-12">
+									<div class=" text-center">
+										<h1 class="hero__title">Synchroconsult</h1>
+										<h1 class="hero__title">Associates LTD</h1>
+										<hr>
+										<h4 class="hero__text">Consultants in Electrical, Mechanical and Information Technology solutions.</h4>
+										<hr>
+									</div>
+								</div>
+							</div>
+							<div class="row m-3">
+								<div class="col-lg-6 text-center">
+									<a href="#" class="btn btn-lg btn-outline-primary 
+								 active" role="button" aria-pressed="true">
+								 <i class="fas fa-arrow-circle-left"></i>
+								 Engineering
+								 <i class="fas fa-tools"></i>
+								</a>
+								</div>
+								<div class="col-lg-6 text-center">
+									<a href="https://software.synchroconsult.co.ke" target="_blank" class="btn btn-lg btn-outline-secondary
+								 active" role="button" aria-pressed="true">
+								 <i class="fas fa-laptop-code"></i>
+								 Software
+								  <i class="fas fa-arrow-circle-right"></i>
+								</a>
+								</div>
+								
+								
+							</div>
+							
+							<div class="service-wrapper mb-3">
+								
+								<!-- service -->
+								<div class="service">
+									<h2 class="service__title">Web/Software Development</h2>
+									<p class="service__text">Development in web based systems</p>
+								</div><!-- End / service -->
+								
+								
+								<!-- service -->
+								<div class="service">
+									<h2 class="service__title">Electrical Engineering Consultancy</h2>
+									<p class="service__text"></p>
+								</div><!-- End / service -->
+								
+								
+								<!-- service -->
+								<div class="service">
+									<h2 class="service__title">Mechanical Engineering Consultancy</h2>
+									<p class="service__text"></p>
+								</div><!-- End / service -->
+								
+							</div>
+						</div>
+					</div>
+				</div><!-- End / hero -->
+				
+			</div>
+			<!-- End / Content-->
+		</div>
+		<script type="text/javascript" src="{{ asset('js/main.js')}}"></script>
+	</body>
+</html>
